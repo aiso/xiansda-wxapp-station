679dc5fd571f13e42ce8934fdb75627f31b4cc89
aiso <aisobubu@gmail.com>
2016-11-23 23:43:47 +0800

    2016-11-23.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,51 +7,8 @@ var Promise = require("../../utils/bluebird.min")
 
 Page({
   data: {
-    access:false,
-    retry:false,
-    welcome: '正在登录鲜时达...',
-    userInfo: {},
-  },
-  login(){
-    this.setData({
-      welcome: '正在登录鲜时达...',
-      retry:false
-    })
-
-    wx.showNavigationBarLoading()
-
-    app.getUserInfo().then(userInfo=>{
-      this.setData({userInfo})
-      return {userInfo, accessCode:app.globalData.accessCode}
-    }).then(params=>{
-      const postData = {code: params.accessCode, userInfo:params.userInfo}
-      postData.code='station-test1' //测试用
-      return xsd.api.post('station/login', postData).then(data=>{
-        wx.hideNavigationBarLoading()
-        if(!!data.user){
-          this.setData({
-            welcome:'正在跳转...'
-          })
-          xsd.station.login(data.user)
-        }else{
-          this.setData({
-            access:true,
-            retry:false,
-            welcome:'无效用户'
-          })
-        }
-      })
-
-    }).catch(err=>{
-      wx.hideNavigationBarLoading()
-      this.setData({
-        welcome:err,
-        retry:true
-      })
-    })
   },
   onLoad: function () {
-    console.log('onLoad')
-    this.login()
+    xsd.station.auth()
   }
 })
